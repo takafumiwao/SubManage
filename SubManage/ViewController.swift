e34be8b958699599845684737c6a9aa7d13f26b8
takafumiwao <koshiiwao2904@icloud.com>
2020-09-24 16:39:19 +0900

    Initial Commit

---
+++ SubManage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubManage
+//
+//  Created by 岩男高史 on 2020/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

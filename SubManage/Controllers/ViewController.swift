c99e7ad68b4ab8fc5e69492026e83ac1b3fade2f
takafumiwao <koshiiwao2904@icloud.com>
2020-09-26 14:06:32 +0900

    1-カレンダーを作成

--- SubManage/Controllers/ViewController.swift
+++ SubManage/Controllers/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 import ReactiveSwift
+import FSCalendar
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak private var scrollView: UIScrollView!
+    @IBOutlet weak private var calender: FSCalendar!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
+        // カレンダーの曜日を日本語にする
+        calender.calendarWeekdayView.weekdayLabels[0].text = "日"
+        calender.calendarWeekdayView.weekdayLabels[1].text = "月"
+        calender.calendarWeekdayView.weekdayLabels[2].text = "火"
+        calender.calendarWeekdayView.weekdayLabels[3].text = "水"
+        calender.calendarWeekdayView.weekdayLabels[4].text = "木"
+        calender.calendarWeekdayView.weekdayLabels[5].text = "金"
+        calender.calendarWeekdayView.weekdayLabels[6].text = "土"
     }
 
 
